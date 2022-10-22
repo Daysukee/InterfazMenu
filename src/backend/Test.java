9f058e078f197195737d8356c46633fbb4fdf7c6
addliam <appplecdos504@gmail.com>
2022-10-21 20:28:56 -0500

    paquete backend prueba

---
+++ src/backend/Test.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package backend;
+
+/**
+ *
+ * @author codeli4m
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Test {
+    
+    static final String DB_URL =
+    "jdbc:mysql://localhost:3306/inventario_farmacia";
+    static final String DB_DRV = "com.mysql.jdbc.Driver";
+    static final String DB_USER = "root";
+    static final String DB_PASSWD = "newpasswordmysql";
+
+   public static void main(String[] args){
+       
+      System.out.println("Test corriendo desde backend paquete");
+
+      Connection connection = null;
+      Statement statement = null;
+      ResultSet resultSet = null;
+
+      try{
+         connection=DriverManager.getConnection
+            (DB_URL,DB_USER,DB_PASSWD);
+         statement=connection.createStatement();
+         resultSet=statement.executeQuery
+            ("SELECT * FROM books");
+         while(resultSet.next()){
+            System.out.printf("%s\t%s\t%s\t%f\n",
+            resultSet.getString(1),
+            resultSet.getString(2),
+            resultSet.getString(3),
+            resultSet.getFloat(4));
+         }
+
+      }catch(SQLException ex){
+      }finally{
+         try {
+            resultSet.close();
+            statement.close();
+            connection.close();
+         } catch (SQLException ex) {
+         }
+      }
+   }
+//    <!-- https://mvnrepository.com/artifact/mysql/mysql-connector-java -->
+//<dependency>
+//    <groupId>mysql</groupId>
+//    <artifactId>mysql-connector-java</artifactId>
+//    <version>8.0.30</version>
+//</dependency>
+
+}
