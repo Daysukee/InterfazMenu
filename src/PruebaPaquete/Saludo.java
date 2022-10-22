7a7097e39843e5c990187e447dc4408f13a90d90
addliam <appplecdos504@gmail.com>
2022-10-21 19:35:03 -0500

    saludo desde prueba paquete

---
+++ src/PruebaPaquete/Saludo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PruebaPaquete;
+
+/**
+ *
+ * @author codeli4m
+ */
+public class Saludo {
+    public static void main(String[] args) {
+        System.out.println("HOLA DESDE Saludo.java");
+    }
+}
